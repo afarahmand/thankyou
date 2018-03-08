338867aa848a5ffb9fd32a1ce055add7e00d4a78
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-03-08 15:43:15 -0800

    Added orders controller (creates a note in db whenever a new order is POSTed)

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,6 +3,11 @@ class Api::OrdersController < ApplicationController
     @Note = Note.new
     @Customer = Customer.find_by(id: params[:customer_id])
 
-    @Note.customer_name = params.
+    @Note.customer_name = @Customer.first_name + @Customer.last_name
+    @Note.child_name = params[:child_name]
+    @Note.body = ""
+    @Note.image_url = ""
+
+    @Note.save
   end
 end
