7527c3901c4cd8ad251f49ea9efa2093a57f175a
Areej <areejht@gmail.com>
2018-03-08 15:31:13 -0800

    set up react frontend

---
+++ frontend/components/thank_you_form_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import ThankYouForm from "./all_movies";
+
+const mapStateToProps = (state, ownProps) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ThankYouForm);
---
+++ frontend/reducers/rootreducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import entitiesReducer from "./entities_reducer";
+// import uiReducer from "./ui_reducer";
+
+const rootReducer = combineReducers({
+  entities: "hello"
+});
+
+export default rootReducer;
