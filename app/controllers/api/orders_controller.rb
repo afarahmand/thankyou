a5fec5effb5e2e30d387c2d2aa7af3eacbd03fb4
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-03-08 15:38:44 -0800

    Added customers migration and model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,4 +1,8 @@
 class Api::OrdersController < ApplicationController
   def create
+    @Note = Note.new
+    @Customer = Customer.find_by(id: params[:customer_id])
+
+    @Note.customer_name = params.
   end
 end
