2836e4b4f8d0029a49694ff4db64c06c612d606e
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-03-08 15:55:38 -0800

    Added notes controller (show, update)

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,7 +1,17 @@
 class Api::NotesController < ApplicationController
   def update
+    @Note = Note.find_by(id: params[:id])
+
+    if @Note
+      if @Note.update_attributes(params)
+        render "api/notes/show"
+      end
+    end
   end
 
   def show
+    @Note = Note.find_by(id: params[:id])
+
+    render "api/notes/show"
   end
 end
